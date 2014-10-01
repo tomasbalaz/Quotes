c26d62dea76932863e5394206b4c2fd1e7dc5bd7
tomasbalaz <balaztomas0@gmail.com>
2014-10-01 14:05:46 +0200

    Print another Hello World

--- src/main/java/org/quote/quotes/Runner.java
+++ src/main/java/org/quote/quotes/Runner.java
@@ -17,6 +17,8 @@ public class Runner {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello world");
+        
+        System.out.println("Hello world");
     }
     
 }
